fe62a447332fc21f2b368fdf5a13795f7615883a
sashaivanovaPro <sashaivanova.pro@gmail.com>
2023-02-02 21:37:51 +0300

    feat: add order links to contacts section

--- plants/js/accordeon.js
+++ plants/js/accordeon.js
@@ -1,9 +1,16 @@
 const headers = document.querySelectorAll('[data-name="accordeon-title"]');
-console.log(headers);
+
 
 headers.forEach( function(item){
-    console.log(item);
+
     item.addEventListener('click', function(){
    this.nextElementSibling.classList.toggle('hidden');
+    })
+})
+
+disappear.forEach(function(item){
+
+    item.addEventListener('click', function(){
+        this.previousElementSibling.classList.toggle('disappear');
     })
 })
