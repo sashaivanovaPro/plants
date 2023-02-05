3324e984647abc75418609ffc9130cd60fc803a3
sashaivanovaPro <sashaivanova.pro@gmail.com>
2023-02-05 22:49:51 +0300

    feat: add dropdown effects

--- plants/js/dropdown.js
+++ plants/js/dropdown.js
@@ -1,3 +1,17 @@
 document.querySelector('.dropdown__button').addEventListener('click', function(){
     document.querySelector('.dropdown__list').classList.toggle('dropdown__list--active');
 });
+
+document.querySelectorAll('.dropdown__list--item').forEach(function(listItem){
+    listItem.addEventListener('click', function(){
+        document.querySelector('.dropdown__button').innerText = this.innerText;
+        document.querySelector('.dropdown__list').classList.remove('dropdown__list--active');
+    })
+})
+
+/* не могу отодвинуть название города на кнопке
+document.querySelectorAll('.dropdown__list--item').forEach(function(listItem){
+    listItem.addEventListener('click', function(){
+        document.querySelector('.dropdown__list--item').style.display = 'block';
+    })
+})*/
