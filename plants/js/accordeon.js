b9e910d63835940d3e123f96e5d78d2120a71e81
sashaivanovaPro <sashaivanova.pro@gmail.com>
2023-02-06 00:31:51 +0300

    fix: work of prices cards

--- plants/js/accordeon.js
+++ plants/js/accordeon.js
@@ -1,4 +1,36 @@
-const headers = document.querySelectorAll('[data-name="accordeon-title"]');
+
+
+document.querySelector('.prices__options--item1').addEventListener('click', function(){
+    document.querySelector('.price__card1').classList.add('visible');
+    document.querySelector('.prices__options--item1').classList.add('hidden');
+});
+
+document.querySelector('.price__card1').addEventListener('click', function(){
+    document.querySelector('.price__card1').classList.remove('visible');
+    document.querySelector('.prices__options--item1').classList.remove('hidden');
+});
+
+document.querySelector('.prices__options--item2').addEventListener('click', function(){
+    document.querySelector('.price__card2').classList.add('visible');
+    document.querySelector('.prices__options--item2').classList.add('hidden');
+});
+
+document.querySelector('.price__card2').addEventListener('click', function(){
+    document.querySelector('.price__card2').classList.remove('visible');
+    document.querySelector('.prices__options--item2').classList.remove('hidden');
+});
+
+document.querySelector('.prices__options--item3').addEventListener('click', function(){
+    document.querySelector('.price__card3').classList.add('visible');
+    document.querySelector('.prices__options--item3').classList.add('hidden');
+});
+
+document.querySelector('.price__card3').addEventListener('click', function(){
+    document.querySelector('.price__card3').classList.remove('visible');
+    document.querySelector('.prices__options--item3').classList.remove('hidden');
+});
+
+/*const headers = document.querySelectorAll('[data-name="accordeon-title"]');
 
 
 headers.forEach( function(item){
@@ -6,7 +38,6 @@ headers.forEach( function(item){
    this.nextElementSibling.classList.toggle('hidden');
     })
 })
-/*
 disappear.forEach(function(item){
 
     item.addEventListener('click', function(){
