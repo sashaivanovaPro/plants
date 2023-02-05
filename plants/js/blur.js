1434a29d9d71610d76892deb33d456f853baa008
sashaivanovaPro <sashaivanova.pro@gmail.com>
2023-02-05 18:38:24 +0300

    feat: add single button service blur

--- plants/js/blur.js
+++ plants/js/blur.js
@@ -6,18 +6,31 @@ document.getElementById('gardens').addEventListener('click', function (){
 })
 })
 
+document.getElementById('lawn').addEventListener('click', function (){
+    const gardenBlur = document.querySelectorAll('.lawn__blur');
+    gardenBlur.forEach(function(item){
+       item.classList.toggle('services__blur');
+})
+})
+
+document.getElementById('planting').addEventListener('click', function (){
+    const gardenBlur = document.querySelectorAll('.plant__blur');
+    gardenBlur.forEach(function(item){
+       item.classList.toggle('services__blur');
+})
+})
 
 /(function () {
     const buttonClickOne = document.getElementById("gardens");
     const buttonClickTwo = document.getElementById("lawn");
     const buttonClickThree = document.getElementById("planting");
         buttonClickOne.addEventListener('click', () => {
-        buttonClick.classList.toggle('service__button--active');
+        buttonClickOne.classList.toggle('service__button--active');
      });
-     buttonClickTwo.addEventListener('click', () => {
+        buttonClickTwo.addEventListener('click', () => {
         buttonClickTwo.classList.toggle('service__button--active');
      });
-     buttonClickThree.addEventListener('click', () => {
+        buttonClickThree.addEventListener('click', () => {
         buttonClickThree.classList.toggle('service__button--active');
      });
     }());
